dc46b68c60ab4a2e5c8be4a5c72fdf77a483da02
Muhammad-saad-2000 <mohamedmiogh2@gmail.com>
2022-05-19 18:04:18 +0200

    Fix e=2 only error

--- 2 - RSAWorld/library.py
+++ 2 - RSAWorld/library.py
@@ -1,7 +1,13 @@
+import pipes
 from ipynb.fs.full.Library import GCD, ModInv, IsPrime, GenRandPrime, Encrypt, Decrypt, log, floor, ConvertToStr, ConvertToInt, ModExp
 
 def sign_up(p, q, e):
    BITSNUM=128
+   err_msg = ''
+   e_inv = 1
+   n = 1
+   phi_n = 1
+   d = 1
    if p == '':
       if e=='':
          if q == '':
@@ -18,6 +24,8 @@ def sign_up(p, q, e):
                p = str(GenRandPrime(int(log(int(q), 2)+1)))
                while GCD(int(p)-1, int(e)) != 1:
                   p = str(GenRandPrime(int(log(int(q), 2)+1)))
+         else:
+            err_msg = 'e must be odd'
 
    if q == '':
       if e=='':
@@ -35,50 +43,52 @@ def sign_up(p, q, e):
                q = str(GenRandPrime(int(log(int(p), 2)+1)))
                while GCD(int(q)-1, int(e)) != 1:
                   q = str(GenRandPrime(int(log(int(p), 2)+1)))
+         else:
+            err_msg = 'e must be odd'
 
 
-   err_msg = ''
    if not p.isdigit() and err_msg == '':
       err_msg = 'p must be a number'
    if not q.isdigit() and err_msg == '':
       err_msg = 'q must be a number'
+   
+   if err_msg == '':
+      p = int(p)
+      q = int(q)
+      n = p*q
+      phi_n = (p - 1) * (q - 1)
 
-   p = int(p)
-   q = int(q)
-   n = p*q
-   phi_n = (p - 1) * (q - 1)
+      if not IsPrime(p) and err_msg == '':
+         err_msg = 'p is not prime'
+      if not IsPrime(q) and err_msg == '':
+         err_msg = 'q is not prime'
+      if p == 2 and err_msg == '':
+         err_msg = 'p must be greater than 2'
+      if q == 2 and err_msg == '':
+         err_msg = 'q must be greater than 2'
+         
+      if e == '':
+         e = str(GenRandPrime(int(log(phi_n,2))))
 
-   if not IsPrime(p) and err_msg == '':
-      err_msg = 'p is not prime'
-   if not IsPrime(q) and err_msg == '':
-      err_msg = 'q is not prime'
-   if p == 2 and err_msg == '':
-      err_msg = 'p must be greater than 2'
-   if q == 2 and err_msg == '':
-      err_msg = 'q must be greater than 2'
-      
-   if e == '':
-      e = str(GenRandPrime(int(log(phi_n,2))))
+      if not e.isdigit() and err_msg == '':
+         err_msg = 'e must be a number'
+      e = int(e)
+      if e%2 == 0 and err_msg == '':
+         err_msg = 'e must be odd'
+      if e <= 1  and err_msg == '':
+         err_msg = 'e should be greater than 1'
+      if e >= phi_n and err_msg == '':
+         err_msg = 'e should be less than φ(n)'
+      if GCD(e, phi_n) != 1 and err_msg == '':
+         err_msg = 'e is not coprime with φ(n)'
 
-   if not e.isdigit() and err_msg == '':
-      err_msg = 'e must be a number'
-   e = int(e)
-   if e%2 == 0 and err_msg == '':
-      err_msg = 'e must be odd'
-   if e <= 1  and err_msg == '':
-      err_msg = 'e should be greater than 1'
-   if e >= phi_n and err_msg == '':
-      err_msg = 'e should be less than φ(n)'
-   if GCD(e, phi_n) != 1 and err_msg == '':
-      err_msg = 'e is not coprime with φ(n)'
+      #?That is isn't reqired but it is a good idea to check it.
+      #TODO:Should we leave it?
+      if p == q and err_msg == '':
+         err_msg = 'p and q are the same that is a bad idea'
 
-   #?That is isn't reqired but it is a good idea to check it.
-   #TODO:Should we leave it?
-   if p == q and err_msg == '':
-      err_msg = 'p and q are the same that is a bad idea'
-
-   d = ModInv(e, phi_n)
-   e_inv = d + n
+      d = ModInv(e, phi_n)
+      e_inv = d + n
    return p, q, e, e_inv, n, phi_n, d, err_msg
 
 
